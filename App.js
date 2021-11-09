2a704ae23e6651a06c72ebefd4a5639bed79e09e
hericklima22 <herick.lima77@gmail.com>
2021-11-09 17:19:44 -0300

    estilização condicional

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params';
+import Field from './src/components/Field';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Iniciando o campo minado!</Text>
       <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
+      
+      <Field />
+      <Field opened />
+      <Field opened nearMines={1} />
+      <Field opened nearMines={2} />
+      <Field opened nearMines={3} />
+      <Field opened nearMines={6} />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Field.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import params from "../params";
+
+export default props => {
+    const { mined, opened, nearMines} = props
+
+    const styleField = [styles.field]
+    if(opened) styleField.push(styles.opened)
+    if(styleField.length === 1) styleField.push(styles.regular)
+
+    let color = null
+    if(nearMines > 0) {
+        if(nearMines == 1) color = '#2a28d7'
+        if(nearMines == 2) color = '#2b520f'
+        if(nearMines > 2 && nearMines < 6) color = '#f9060a'
+        if(nearMines >= 6) color = '#f221a9'
+    }
+
+    return (
+        <View style={styleField}>
+            {!mined && opened && nearMines > 0 ? 
+            <Text style={[styles.label, {color}]}>
+                {nearMines}
+            </Text> : false
+            }
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    field: {
+        height: params.blockSize,
+        width: params.blockSize,
+        borderWidth: params.borderSize,
+    },
+    regular: {
+        backgroundColor: '#999',
+        borderLeftColor: '#ccc',
+        borderTopColor: '#ccc',
+        borderRightColor: '#333',
+        borderBottomColor: '#333'
+    },
+    opened: {
+        backgroundColor: '#999',
+        borderColor: '#777',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    label: {
+        fontWeight: 'bold',
+        fontSize: params.fontSize,
+    }
+})
