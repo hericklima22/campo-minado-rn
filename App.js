e40404eda6373ed139ec3f56f0042c319f58d3e5
hericklima22 <herick.lima77@gmail.com>
2021-11-09 22:41:49 -0300

    campo minado implementado e funcionando

--- App.js
+++ App.js
@@ -1,59 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { Component, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import params from './src/params';
-import MineField from './src/components/MineField';
-import { createMinedBoard } from './src/functions';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Alert } from "react-native";
+import params from "./src/params";
+import MineField from "./src/components/MineField";
+import {
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines,
+} from "./src/functions";
+import Field from "./src/components/Field";
 
 export default class App extends Component {
-
   constructor(props) {
-    super(props)
-    this.state = this.createState()
+    super(props);
+    this.state = this.createState();
   }
 
   minesAmount = () => {
-    const cols = params.getColumnsAmount()
-    const rows = params.getRowsAmount()
-    return Math.ceil(cols * rows * params.difficultLevel)
-  }
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return Math.ceil(cols * rows * params.difficultLevel);
+  };
 
   createState = () => {
-    const cols = params.getColumnsAmount()
-    const rows = params.getRowsAmount()
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
     return {
-      board: createMinedBoard(rows, cols, this.minesAmount())
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    };
+  };
+
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board);
+    openField(board, row, column);
+    const lost = hadExplosion(board);
+    const won = wonGame(board);
+
+    if (lost) {
+      showMines(board);
+      Alert.alert("Perdeu burrao");
     }
-  }
+
+    if (won) {
+      Alert.alert("Ganhoooooou");
+    }
+
+    this.setState({ board, lost, won });
+  };
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Iniciando o campo minado!</Text>
-        <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
-        
+        <Text style={styles.instructions}>
+          Tamanho da grade: {params.getRowsAmount()} x{" "}
+          {params.getColumnsAmount()}
+        </Text>
         <View styles={styles.board}>
-          <MineField board={this.state.board} />
+          <MineField board={this.state.board} onOpenField={this.onOpenField} />
+          {/* <Field /> */}
         </View>
         <StatusBar style="auto" />
       </View>
     );
   }
-
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   board: {
-    alignItems: 'center',
-    backgroundColor: '#aaa' 
+    alignItems: "center",
+    backgroundColor: "#aaa",
   },
   welcome: {
-    textAlign: 'center',
-    fontSize: 20
-  }
+    textAlign: "center",
+    fontSize: 20,
+  },
 });
--- src/components/MineField.js
+++ src/components/MineField.js
@@ -2,18 +2,24 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import Field from "./Field";
 
-export default props => {
-    const rows = props.board.map((row, r) => {
-        const columns = row.map((field, c) => {
-            return <Field {...field} key={c} />
-        })
-        return <View key={r} style={{flexDirection: 'row'}}>{columns}</View>
-    })
-    return <View style={styles.container}>{rows}</View>
-}
+export default (props) => {
+  const rows = props.board.map((row, r) => {
+    const columns = row.map((fields, c) => {
+      return (
+        <Field {...fields} key={c} onOpen={() => props.onOpenField(r, c)} />
+      );
+    });
+    return (
+      <View key={r} style={{ flexDirection: "row" }}>
+        {columns}
+      </View>
+    );
+  });
+  return <View style={styles.container}>{rows}</View>;
+};
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#eee'
-    }
-})
+  container: {
+    backgroundColor: "#eee",
+  },
+});
